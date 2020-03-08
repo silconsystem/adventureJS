b3892c52d1a3fa6dc97332dd8b00ae2606168fc6
silconsystem <silcon.art.system@gmail.com>
2020-03-08 18:44:27 +0100

    working on inventory handler

--- js/action-handles.js
+++ js/action-handles.js
@@ -44,10 +44,11 @@ function textParser(txt) {
 		action_obj;									// if found create 'item_obj' for further sorting
 
 	// create a base object from found item to use for later handling
-	function item_obj(name, type, handler, exists) {
+	function item_obj(name, type, handler, url, exists) {
 		this.name 		= name;						// name
 		this.type 		= type;						// type: item|weapon|magic|npc|enemy|event
 		this.handler 	= handler;					// used by actor: player|thief|warrior|rogue|mage
+		this.url 		= url;						// the image url
 		this.exists 	= exists;					// set exist flag 
 	}
 
@@ -75,7 +76,8 @@ function textParser(txt) {
 	function getWeaponValue(itm) {
 
 		var result,
-			handler;
+			handler,
+			url;
 
 		for (let i in game_obj[1].weapon[0].thief[0].name) {
 
@@ -85,24 +87,28 @@ function textParser(txt) {
 
 				obj_exists 	= true;
 				handler 	= 'thief';
+				url 		= game_obj[1].weapon[0].thief[i].url;
 				result 		= game_obj[1].weapon[0].thief[i].name;
 				break;				
 			} else if (obj_exists == false) {
 
 				obj_exists 	= true;
 				handler 	= 'warrior';
-				result 		= game_obj[1].weapon[0].thief[i].name;
+				url 		= game_obj[1].weapon[1].warrior[i].url;
+				result 		= game_obj[1].weapon[1].warrior[i].name;
 				break;
 			} else if (game_obj[1].weapon[2].rogue[i].name.includes(itm)) {
 				
 				obj_exists 	= true;
 				handler 	= 'rogue';
+				url 		= game_obj[1].weapon[2].rogue[i].url;
 				result 		= game_obj[1].weapon[2].rogue[i].name;
 				break;
 			} else if (game_obj[1].weapon[3].mage[i].name.includes(itm)) {
 				
 				obj_exists 	= true;
-				handler 	= 'mage';
+				handler 	= 'mage';				
+				url 		= game_obj[1].weapon[3].mage[i].url;
 				result 		= game_obj[1].weapon[3].mage[i].name;
 				break;
 			}
@@ -118,7 +124,7 @@ function textParser(txt) {
 
 			// set type and create new item_obj
 			obj_type = 'weapon';
-	        itm = new item_obj(result, obj_type, handler, obj_exists);
+	        itm = new item_obj(result, obj_type, handler, url, obj_exists);
 		}
 		console.log('getItemValue: returning input:', itm, 'of type:',obj_type);
 		return itm;	
@@ -127,7 +133,8 @@ function textParser(txt) {
 	// test if target is an item or not
 	function getItemValue(itm) {
 
-		var result;
+		var result,
+			url;
 
 		// find match and set obj_exists to true
 		for (let i in game_obj[0].item[0].name) {
@@ -135,8 +142,9 @@ function textParser(txt) {
 			console.log(game_obj[0].item[i].name);
 		    if (game_obj[0].item[i].name.includes(itm)) {
 
-		    	obj_exists = true;
-		    	result = game_obj[0].item[i].name;
+		    	obj_exists 	= true;
+		    	url 		= game_obj[0].item[i].url;
+		    	result 		= game_obj[0].item[i].name;
 		        break;
 		    } 
 		}
@@ -150,9 +158,10 @@ function textParser(txt) {
 
 		} else {
 
-			handler = 'player';
-			obj_type = 'item';
-	        itm = new item_obj(result, obj_type, handler, obj_exists);
+			handler 	= 'player';
+			obj_type 	= 'item';
+			url 		= url;
+	        itm = new item_obj(result, obj_type, handler, url, obj_exists);
 	        console.log('getItemValue: target found, input:', itm.name, 'result:', result);
 	        console.log('getItemValue: returning new object:', itm);
 		}
@@ -164,11 +173,12 @@ function textParser(txt) {
 	if (getItemValue(target)) {
 
 		action_obj = getItemValue(target);
-		console.log('getItemValue: target value is now:', action_obj);
-		console.log('getItemValue: target name:', action_obj.name);
-		console.log('getItemValue: target name:', action_obj.type);
-		console.log('getItemValue: target name:', action_obj.handler);
-		console.log('getItemValue: target name:', action_obj.exists);
+		console.log('getItemValue: target value is now:'	, action_obj);
+		console.log('getItemValue: target name:'			, action_obj.name);
+		console.log('getItemValue: target type:'			, action_obj.type);
+		console.log('getItemValue: target handler:'			, action_obj.handler);
+		console.log('getItemValue: target url:'				, action_obj.url);
+		console.log('getItemValue: target exists:'			, action_obj.exists);
 
 		if (handleInventory(cmd, action_obj)) {
 
@@ -179,18 +189,19 @@ function textParser(txt) {
 	} else if (getWeaponValue(target)) {
 
 		action_obj = getWeaponValue(target);
-		console.log('getWeaponValue: target value is now:', action_obj);
-		console.log('getWeaponValue: target name:', action_obj.name);
-		console.log('getWeaponValue: target name:', action_obj.type);
-		console.log('getWeaponValue: target name:', action_obj.handler);
-		console.log('getWeaponValue: target name:', action_obj.exists);
+		console.log('getWeaponValue: target value is now:'	, action_obj);
+		console.log('getWeaponValue: target name:'			, action_obj.name);
+		console.log('getWeaponValue: target type:'			, action_obj.type);
+		console.log('getWeaponValue: target handler:'		, action_obj.handler);
+		console.log('getWeaponValue: target url:'			, action_obj.url);
+		console.log('getWeaponValue: target exists:'		, action_obj.exists);
 
 		if (handleInventory(cmd, action_obj)) {
 
 			console.log('item', action_obj.name,'succesfully added to player inventory');
 			console.log(player.inventory.items);
 		}
-		
+
 	} else {
 		
 		console.log('getItemValue returned false');
@@ -222,6 +233,7 @@ function textParser(txt) {
 		console.log('handleInventory: item object:', itemObj);
 		console.log(itemObj.name);
 		console.log(itemObj.type);
+		console.log(itemObj.url);
 		console.log(itemObj.exists);
 
 		// take or add 
@@ -229,12 +241,26 @@ function textParser(txt) {
 			case "take":
 
 				// add item to inventory
-				manInventory(0, itemObj.type, itemObj.name);
+				manInventory(0, itemObj.type, itemObj);
+
+				switch (itemObj.type) {
+					case "item":
+						// statements_1
+						document.getElementById('item-image').innerHTML = '<img src=\"' + itemObj.url + '\">';	
+						break;
+					case "weapon":
+
+						// statements_def
+						document.getElementById('weapon-image').innerHTML = '<img src=\"' + itemObj.url + '\">';
+						break;
+				}
+
 				break;
 			case "drop":
 				
 				// drop item from inventory
-				manInventory(1, itemObj.type, itemObj.name);
+				manInventory(1, itemObj.type, itemObj);
+				document.getElementById('inventory-weapon').innerHTML = '<img src=\"../img/weapons/nothing.png\">';
 				break;
 			default:
 
