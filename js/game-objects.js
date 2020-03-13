c72e8c5c4bc34bf2208f6be0e84abf66fe565394
silconsystem <silcon.art.system@gmail.com>
2020-03-13 22:23:00 +0100

    textparser and newrow: images not all in wraps, c_create img not done

--- js/game-objects.js
+++ js/game-objects.js
@@ -1,95 +1,21 @@
-/**const game_obj = [			/* 			This is the new way I'm gonna structure the data(if it works like I want)		
-	{
-		item: 
-		[
-			{name:"item",	 	description:"player",		 	game:"HP - 0",	 	url: "../img/weapons/nothing.png", 		type: "item"},
-			{name:"potion",		description:"small boost",		game:"HP +10",	 	url: "../img/items/item/potion.png",	type: "item"},
-			{name:"ether",		description:"MP boost",		 	game:"MP +10",	 	url: "../img/items/item/ether.png",		type: "item"},
-			{name:"revive",		description:"alive",			game:"HP +100", 	url: "../img/items/item/revive.png",	type: "item"},
-			{name:"helmet",		description:"protect head",	 	game:"evd +3",	 	url: "../img/items/item/helmet.png", 	type: "item"},
-			{name:"coins",		description:"player coin",		game:"coins +10", 	url: "../img/items/item/coins.png", 	type: "item"},
-			{name:"medals",		description:"luck plus",		game:"luc +5", 		url: "../img/items/item/medals.png", 	type: "item"},
-			{name:"crown",		description:"strenght plus",	game:"str +5", 		url: "../img/items/item/crown.png", 	type: "item"},
-			{name:"robes",		description:"strenght plus",	game:"str +1", 		url: "../img/items/item/robes.png", 	type: "item"}
-		]
-	},
-	{
-		weapon: 
-		[
-			{
-				thief:
-				[
-					{name: "weapon", 		description: "player-thief",	game: "HP -0", 		url: "../img/weapons/nothing.png"},
-					{name: "stick",			description: "a strong stick",	game: "HP -5", 		url: "../img/weapons/thief/stick.png"},
-					{name: "throwknife",	description: "small and deadly",game: "HP -5", 		url: "../img/weapons/thief/throwknife.png"},
-					{name: "cane",			description: "pointy cane", 	game: "HP -8", 		url: "../img/weapons/thief/cane.png"},
-					{name: "whip",			description: "longrange",		game: "HP -10",		url: "../img/weapons/thief/whip.png"}
-				]
-			},
-			{
-				warrior:
-				[
-					{name: "weapon", 		description: "player-warrior",	game: "HP -0", 		url: "../img/weapons/nothing.png"},
-					{name: "club",			description: "a wood club",		game: "HP -6", 		url: "../img/weapons/warrior/club.png"},
-					{name: "mace",			description: "smash heads",		game: "HP -7", 		url: "../img/weapons/warrior/mace.png"},
-					{name: "chains",		description: "brute force", 	game: "HP -8", 		url: "../img/weapons/warrior/chains.png"},
-					{name: "trident",		description: "shortrange",		game: "HP -10",		url: "../img/weapons/warrior/trident.png"}
-				]		
-			},
-			{
-				rogue:
-				[
-					{name: "weapon", 		description: "player-rogue", 	game: "HP -0", 		url: "../img/weapons/nothing.png"},
-					{name: "dagger",		description: "simple dagger", 	game: "HP -6",		url: "../img/weapons/rogue/dagger.png"},
-					{name: "sword",			description: "sharp and deadly",game: "HP -7",		url: "../img/weapons/rogue/sword.png"},
-					{name: "rapier", 		description: "elegance",		game: "HP -8",		url: "../img/weapons/rogue/rapier.png"},
-					{name: "longsword", 	description: "more range",		game: "HP -10",		url: "../img/weapons/rogue/longsword.png"}
-				]	
-			},
-			{
-				mage:
-				[
-					{name: "weapon", 		description: "player-mage",		game: "HP -0", 		url: "../img/weapons/nothing.png"},
-					{name: "woodstaff",		description: "walking staff",	game: "HP -6", 		url: "../img/weapons/mage/woodstaff.png"},
-					{name: "copperstaff",	description: "simple staff",	game: "HP -7", 		url: "../img/weapons/mage/copperstaff.png"},
-					{name: "ironstaff",		description: "strong staff", 	game: "HP -8", 		url: "../img/weapons/mage/ironstaff.png"},
-					{name: "silverstaff",	description: "fancy staff",		game: "HP -10",		url: "../img/weapons/mage/silverstaff.png"}
-				]
-			}
-		]
-	},
-	{
-		magic_D:
-		[
-			{name: "magic_D", 	description: "player-magic-d",	game: "HP -0", 		url: "../img/weapons/nothing.png"},
-			{name: "fire", 		description: "fire spell", 		game: "HP -10", 	url: "../img/magic/magic_D/fire.png"},
-			{name: "ice", 		description: "ice spell", 		game: "HP -10", 	url: "../img/magic/magic_D/ice.png"},
-			{name: "bolt", 		description: "thunder spell", 	game: "HP -11", 	url: "../img/magic/magic_D/bolt.png"},
-			{name: "plague", 	description: "poison spell", 	game: "HP -1", 		url: "../img/magic/magic_D/plague.png"}
-		]
-	},
-	{
-		magic_H:
-		[
-			{name: "Magic_H", 	description: "player-magic-h", 	game: "HP -0", 		url: "../img/weapons/nothing.png"},
-			{name: "cure", 		description: "cure HP spell",	game: "HP + 20",	url: "../img/magic/magic_H/cure.png"},
-			{name: "cure-all", 	description: "cure all spell",	game: "HP + 20",	url: "../img/magic/magic_H/cure-all.png"},
-			{name: "life", 		description: "full HP",			game: "HP + 100",	url: "../img/magic/magic_H/life.png"},
-			{name: "antidote", 	description: "stop poison",		game: "ST_P = 0", 	url: "../img/magic/magic_H/antidote.png"}
-		]
-	},
-	{
-		skills:
-		[
-			{name: "skills", 	description: "player-skill",	game: "0", 			url: "../img/weapons/nothing.png"},
-			{name: "1st_attack",description: "magic defense", 	game: "SK_00", 		url: "../img/skills/first-attack.png"},
-			{name: "scan", 		description: "magic defense", 	game: "SK_01", 		url: "../img/skills/scan.png"},
-			{name: "magician", 	description: "magic defense", 	game: "SK_02", 		url: "../img/skills/magician.png"},
-			{name: "berserk", 	description: "magic defense", 	game: "SK_03", 		url: "../img/skills/scan.png"},
-		]
-	}
-];*/
-/* 			This is the new way I'm gonna structure the data(if it works like I want)		*/
+/**
+ * 		Game objects list
+ * 
+ * 		object {
+ * 			name: "name",
+ * 			description: "describe",
+ * 			game: "event",
+ * 			url: "path/to/image.img",
+ * 			type: "item|weapon|magic_D|magic_H|skill"
+ * 			handler: "player|pl_class|NPC/enemy"	
+ * 		}
+ * 
+ * 		access: game_objects[0].item[index_no.].name|description|game|url|type|handler
+ * 				game_objects[1].weapon[index_no.].name|description|game|url|type|handler
+ * 				game_objects[2].magic[index_no.].name|description|game|url|type|handler
+ * 				game_objects[3].item[index_no.].name|description|game|url|type|handler
+ * 
+ */
 const game_objects = [			
 	{
 		item: 
